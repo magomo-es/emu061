aae887e713ce57b06f4339d1a3b5e5dadbdcae85
magomo <mgoncevatt.cep@gmail.com>
2021-03-17 18:17:04 +0100

    20210317.1817

--- Dev/routes/api.php
+++ Dev/routes/api.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RolController;
+use App\Http\Controllers\Api\SexeController;
+use App\Http\Controllers\Api\RecursController;
+use App\Http\Controllers\Api\UsuariController;
+use App\Http\Controllers\Api\AfectatController;
+use App\Http\Controllers\Api\ComarcaController;
+use App\Http\Controllers\Api\AlertantController;
+use App\Http\Controllers\Api\MunicipiController;
+use App\Http\Controllers\Api\ProvinciaController;
+use App\Http\Controllers\Api\IncidenciaController;
+use App\Http\Controllers\Api\TipusRecursController;
+use App\Http\Controllers\Api\TipusAlertantController;
+use App\Http\Controllers\Api\TipusIncidenciaController;
 
 /*
 |--------------------------------------------------------------------------
