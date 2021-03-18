66ca810cc866578afe2a1d876ad510d80d8d08b8
magomo <mgoncevatt.cep@gmail.com>
2021-03-19 00:28:14 +0100

    20210319.0027

---
+++ tmp/js/scripts.js
@@ -0,0 +1,74 @@
+function newBlock(xposition, xbefore, xafter, xcolor, xtitle, xsubtitle, xcontent, xismain) {
+  return bloque = {
+      position: xposition,
+      before: xbefore,
+      after: xafter,
+      color: xcolor,
+      title: xtitle,
+      subtitle: xsubtitle,
+      content: xcontent,
+      ismain: xismain,
+      getTitle: function() { return this.title; },
+      getContent: function() { return this.content; }
+  };
+}
+
+function createBlocks(item, index) {
+  var container = document.getElementsByTagName("BODY")[0];
+  var tmpobj = document.createElement("DIV");
+  tmpobj.id = 'bloque_' + index;
+  tmpobj.classList.add('theblock');
+  if (item.ismain) { tmpobj.classList.add('keyblock'); } else { tmpobj.classList.add('oneblock'); }
+  //tmpobj.addEventListener( "click", function() { changeBox(tmpobj); } );
+  tmpobj.style.zIndex = ((item.ismain)?'10':'0');
+  tmpobj.style.background = 'radial-gradient(circle, ' + item.color + ', #000)';
+  tmpobj.dataset.position = item.position;
+  tmpobj.dataset.before = item.before;
+  tmpobj.dataset.after = item.after;
+  tmpobj.dataset.color = item.color;
+  tmpobj.innerHTML = '<div class="'+((item.ismain)?'mainman':'boxman')+'">'+
+      '<h1 class="'+((item.ismain)?'maintitle':'boxtitle')+'">'+item.title+'</h1>'+
+      '<h6 class="'+((item.ismain)?'mainsubtitle':'boxsubtitle')+'">'+item.subtitle+'</h6>'+
+      '<div class="boxcontent">'+item.content+'</div>'+
+      '</div>'+ 
+      ((tmpobj.dataset.before>=0)?'<button class="navButton prevButton" onClick="prevButton(this.parentElement)">back</button>':'')+
+      ((tmpobj.dataset.after>=0)?'<button class="navButton nextButton" onClick="nextButton(this.parentElement)">next</button>':'');
+
+  container.appendChild(tmpobj);
+  if (item.ismain) { activeBlock = tmpobj; }
+}
+
+function changeBox(theobj) {
+  setTimeout( function(theobj){ 
+      activeBlock.style.zIndex='-1'; 
+      theobj.style.zIndex='10';  
+      activeBlock = theobj; 
+  }, 1000, theobj);
+  theobj.style.opacity = '0';
+}
+
+function nextButton(theobj) {
+  tmpnxt = document.getElementById('bloque_' + theobj.dataset.after);
+  tmpnxt.style.zIndex='5';
+  tmpnxt.style.opacity = '1';
+  setTimeout( function( theobj, tmpnxt ) { 
+      tmpnxt.style.zIndex='10'; 
+      theobj.style.zIndex='0';  
+      activeBlock = tmpnxt; 
+      }, 1000, theobj, tmpnxt
+  );
+  theobj.style.opacity = '0';
+}
+
+function prevButton(theobj) {
+  tmpprv = document.getElementById('bloque_' + theobj.dataset.before);
+  tmpprv.style.zIndex='5';
+  tmpprv.style.opacity = '1';
+  setTimeout( function( theobj, tmpprv ) { 
+      tmpprv.style.zIndex='10'; 
+      theobj.style.zIndex='0';  
+      activeBlock = tmpprv; 
+      }, 1000, theobj, tmpprv
+  );
+  theobj.style.opacity = '0';
+}
